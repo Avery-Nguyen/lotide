ea02b8006f6d0d2445dbe5870f54ea5f60f73d08
Avery Nguyen <avery.vnguyen@gmail.com>
2020-05-26 23:09:02 +0000

    added compare array function

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2){
+  let result = true
+  if(result){
+    arr1.forEach(element => {
+      arr2.forEach(element2 => {
+        if(element === element2){
+          result = true;
+        } else {
+          return result = false;
+        }
+      })
+    });
+  }else {
+    return result = false;
+  }
+  return result;
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
