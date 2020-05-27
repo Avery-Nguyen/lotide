1dbef3ea8ce01ef2c909d1ba5e14b017a430305b
Avery Nguyen <avery.vnguyen@gmail.com>
2020-05-27 21:10:27 +0000

    function added

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, property) {
+  let result = "";
+  const arrKeys = Object.keys(object);
+  for (const key of arrKeys) {
+    if (object[key] === property) {
+      result = key;
+    } else {
+      result = undefined;
+    }
+  }
+  return result;
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
