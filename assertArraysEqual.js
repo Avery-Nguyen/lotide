45fd6e3b0251baabf6b93000a8a6c15baae00cec
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-01 15:11:04 +0000

    function added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays')
+
 const assertArraysEqual = function(arr1, arr2){
   if(eqArrays(arr1, arr2)){
     console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
@@ -6,23 +8,4 @@ const assertArraysEqual = function(arr1, arr2){
   }
 }
 
-const eqArrays = function(arr1, arr2){
-  let result = true
-  if(result){
-    arr1.forEach(element => {
-      arr2.forEach(element2 => {
-        if(element === element2){
-          result = true;
-        } else {
-          return result = false;
-        }
-      })
-    });
-  }
-  return result;
-}
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = assertArraysEqual;
